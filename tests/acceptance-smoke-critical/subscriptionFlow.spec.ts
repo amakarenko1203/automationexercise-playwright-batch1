5ff2db4c16e2550e98730d6f11d4c2d7501dced1
amakarenko1203 <annamakarenko.rdh@gmail.com>
2025-10-01 18:29:02 -0500

    Created a test for subscription

--- tests/acceptance-smoke-critical/subscriptionFlow.spec.ts
+++ tests/acceptance-smoke-critical/subscriptionFlow.spec.ts
@@ -1,4 +1,4 @@
-import {test} from '@playwright/test';
+import { test } from '@playwright/test';
 import { HomePage } from '../../pages/homePage';
 import { SubscriptionPage } from '../../pages/subscriptionPage';
 import { CartPage } from '../../pages/cartPage';
@@ -6,22 +6,23 @@ import { CartPage } from '../../pages/cartPage';
 test.describe('Subscription flow tests', () => {
     let homePage: HomePage;
     let subscriptionPage: SubscriptionPage;
-    test.beforeEach('Setting up preconditions',async ({ page }) => {
+    test.beforeEach('Setting up preconditions', async ({ page }) => {
         homePage = new HomePage(page);
-        subscriptionPage = new SubscriptionPage(page);  
-        const url=process.env.baseURL 
-    await page.goto(url!);
-});
+        subscriptionPage = new SubscriptionPage(page);
+        const url = process.env.baseURL
+        await page.goto(url!);
+    });
 
-test('Verify Subscription in home page', async ({ page }) => {
-    await homePage.scrollToFooter();
-    await homePage.isFooterVisible();
-    await subscriptionPage.subscribe('annamakarenko1996@gmail.com');
-    await subscriptionPage.verifySubscriptionSuccessMessage();
-});
-test('Verify Subscription title in cart page', async ({ page }) => {
-    const cartPage = new CartPage(page);
-    await cartPage.isSubscriptionTitleVisible();
-    await cartPage.subscribeAndValidateSubscription('ggffgf@gmail.com');
+    test('Verify Subscription in home page', async ({ page }) => {
+        await homePage.scrollToFooter();
+        await homePage.isFooterVisible();
+        await subscriptionPage.subscribe('annamakarenko1996@gmail.com');
+        await subscriptionPage.verifySubscriptionSuccessMessage();
+    });
+    test('Verify Subscription title in cart page', async ({ page }) => {
+        const cartPage = new CartPage(page);
+        await cartPage.isSubscriptionTitleVisible();
+        await cartPage.subscribeAndValidateSubscription('ggffgf@gmail.com');
 
+    });
 });
